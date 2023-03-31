7b27c82466d957739d36ae90a274f920f6282c01
qiuhognwei <1029913527@qq.com>
2023-03-31 17:45:58 +0800

    build(*): 添加pinia

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import App from './App.vue';
 import { createApp } from 'vue';
 
 import router from './router/index';
-// import store from './store/index.js';
+import store from './store/index';
 import '@/commons/styles/var.scss';
 
 const app = createApp(App);
-app.use(router).mount('#app');
+
+app.use(router).use(store).mount('#app');
